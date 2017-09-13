2ac0b46a80ef072d2a2b1a752d0beedac563c6dc
megancallenbach <megancallenbach@gmail.com>
2017-09-13 10:38:38 +0200

    Update all todo counters

--- todo.js
+++ todo.js
@@ -1,5 +1,18 @@
-var totalCount = document.getElementById("total-count");
+function updateCounters() {
+  // Updating total number of todos
+  var totalCount = document.getElementById("total-count");
+  var totalTodos = document.getElementsByClassName("todo").length;
+  totalCount.innerHTML = totalTodos;
 
-var totalTodos = document.getElementsByClassName("todo").length;
+  // Updating total number of completed todos
+  var completedCount = document.getElementById("completed-count");
+  var completedTodos = document.getElementsByClassName("completed").length;
+  completedCount.innerHTML = completedTodos;
 
-totalCount.innerHTML = totalTodos;
+  // Updating total number of uncomplete todos
+  var todoCount = document.getElementById("todo-count");
+  var uncompletedTodos = totalTodos - completedTodos;
+  todoCount.innerHTML = uncompletedTodos;
+}
+
+updateCounters();
